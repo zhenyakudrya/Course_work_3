c1793314f6a1820f44dbc1076fe238d1acc7be04
Zhenya Kudrya <megusta997@gmail.com>
2023-09-28 20:16:35 +0300

    Add tests for functions Add requirements

---
+++ tests/test_functions.py
@@ -0,0 +1,36 @@
+from src.functions import *
+import pytest
+import json
+
+
+@pytest.fixture
+def list_fixture():
+    with open('src/operations.json', mode='r', encoding='utf-8') as file:
+        file_json = json.load(file)
+    operations_list = []
+    for item in file_json:
+        if 'id' in item:
+            operations_list.append(item)
+    return operations_list
+
+
+def test_sort_by_date(list_fixture):
+    for i in range(0, len(list_fixture) - 1):
+        assert sort_by_date(list_fixture)[i]['date'] > sort_by_date(list_fixture)[i + 1]['date']
+
+
+def test_executed_operations(list_fixture):
+    for item in executed_operations(list_fixture):
+        assert item["state"] == "EXECUTED"
+
+
+def test_last_operations(list_fixture):
+    assert len(last_operations(list_fixture, 5)) == 5
+
+
+def test_date_conversion(list_fixture):
+    assert date_conversion(list_fixture)[0]['date'] == '26.08.2019'
+
+
+def test_encryption(list_fixture):
+    assert encryption(list_fixture)[0]['from'] == 'Maestro 1596 83** **** 5199'
