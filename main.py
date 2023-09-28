f3e907846aacadf6529a4a9abfcdafda27de96f2
Zhenya Kudrya <megusta997@gmail.com>
2023-09-28 13:22:22 +0300

    first version of the widget

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-import json
+from functions import (load_operations, sort_by_date, executed_operations,
+                       last_operations, date_conversion, encryption)
 
-with open('operations.json', mode='r', encoding='utf-8') as file:
-    operations_json = json.load(file)
+if __name__ == '__main__':
+
+    # Загрузка json файла
+    operations_json = load_operations('operations.json')
+
+    # Сортировка списка операций по дате
+    operations_list = sort_by_date(operations_json)
+
+    # Выбор только выполненных операций
+    operations_list = executed_operations(operations_list)
+
+    # Выбор последних 5 операций
+    operations_list = last_operations(operations_list, 5)
+
+    # Конвертация даты в вид "дд.мм.ггг"
+    operations_list = date_conversion(operations_list)
+
+    # Шифровка данных карт, счетов
+    operations_list = encryption(operations_list)
+
+    # Вывод информации на экран
+    for item in operations_list:
+        if "Перевод" in item["description"]:
+            print(f'{item["date"]} {item["description"]}\n'
+                  f'{item["from"]} -> {item["to"]}\n'
+                  f'{item["operationAmount"]["amount"]} {item["operationAmount"]["currency"]["name"]}\n')
+        elif "Открытие" in item["description"]:
+            print(f'{item["date"]} {item["description"]}\n'
+                  f'{item["to"]}\n'
+                  f'{item["operationAmount"]["amount"]} {item["operationAmount"]["currency"]["name"]}\n')
